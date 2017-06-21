9f5cd7ebc69e43c8704ee1ddbf10868215c6e05d
rorusage <rorusage@gmail.com>
2017-06-21 17:04:33 +0800

    User can CRUD their posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,55 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_post, only: [:show]
 
   def index
-    flash[:alert] = "test"
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post), notice: "新增文章成功！"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post), alert: "更新文章成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, alert: "刪除文章成功！"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, presence: true
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
+
+  def editable_by?(user)
+    user == author
+  end
 end
