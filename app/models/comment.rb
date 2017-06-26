65a7c92bd90f0ca385421e1fa8d65d69a5f52758
rorusage <rorusage@gmail.com>
2017-06-26 16:46:25 +0800

    Add time_ago_in_words, user_signed_in?

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, class_name: "User", foreign_key: :user_id
-  belongs_to :post
+  belongs_to :post, counter_cache: :comments_count
   validates :content, presence: true
 
   def editable_by?(user)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621090558) do
+ActiveRecord::Schema.define(version: 20170626082422) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20170621090558) do
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "comments_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
